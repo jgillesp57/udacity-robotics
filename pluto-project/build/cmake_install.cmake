5dff35fca962fd5d02da2d40fb6be57530696dec
Jon Gillespie <jon.gillesp@gmail.com>
2021-02-26 22:31:22 -0800

    redo make file and add more robots to fit spec

--- pluto-project/build/cmake_install.cmake
+++ pluto-project/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/robond/pluto-project
+# Install script for directory: /home/robond/udacity-robotics/pluto-project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/robond/pluto-project/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/robond/udacity-robotics/pluto-project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
